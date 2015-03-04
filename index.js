7a174630e1ad1408ab610d7ba2534bc9da9793ea
Manthan Dave <manthanhd@live.com>
2015-03-04 19:33:25 +0000

    Dynamic configuration added. Now POSTing an object containing name, port and address will register that proxy.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var http = require('http');
+var express = require('express');
 var fs = require('fs');
+
+var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+
 var map = JSON.parse(fs.readFileSync('host-map.json', 'utf8'));
 map = map.hosts;
 
@@ -9,14 +19,28 @@ if (!map || map.length == 0) {
     console.log(map.length + " hosts found.");
 }
 
+for (var i = 0; i < map.length; i++) {
+    var host = map[i];
+    console.log("Will map " + host.name + " to " + host.address + ":" + host.port + "...");
+}
+
 var server = http.createServer();
 server.addListener("request", requestHandler);
 server.listen(process.env.PORT || 8080);
 
-for (var i = 0; i < map.length; i++) {
-    var host = map[i];
-    console.log("Mapping " + host.name + " to " + host.address + ":" + host.port + "...");
-}
+app.post('/host', function(req, res) {
+    if(!req.body.name || !req.body.port || !req.body.address) {
+        res.send(400);
+        return;
+    }
+
+    map.push({name: req.body.name, address: req.body.address, port: req.body.port});
+    res.send(200);
+})
+
+app.listen(process.env.PORT_CONFIG || 8081, function() {
+    console.log("Configuration server listening.");
+})
 
 function requestHandler(req, res) {
     var vhost = req.headers.host;
@@ -30,7 +54,8 @@ function requestHandler(req, res) {
         var proxy = http.createClient(host.port, host.address);
 
         proxy.addListener("error", function (socketException) {
-            res.end("Unreachable.");
+            res.statusCode = 404;
+            res.end();
         });
 
         req.headers["X-Forwarded-For"] = req.connection.remoteAddress;
@@ -80,8 +105,4 @@ function getSubDomain(hostname) {
     var res = hostname.match(/[\d\w-_]+\./g);
     var host = res[0];
     return host.substring(0, host.length - 1);
-}
-
-function proxyThrough(req, res, host) {
-
 }
